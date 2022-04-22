2d486d4f831d83291065545b8b68605e6507c31a
lazicMichelle <mischilazi@gmail.com>
2022-04-22 15:53:52 +0200

    #27 fe category list, #28 fe journal list --> Controller erstellt, Icons, Pages bearbeitet

---
+++ src/main/java/at/jku/se/diary/JournalListController.java
@@ -0,0 +1,51 @@
+package at.jku.se.diary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class JournalListController {
+
+    @FXML
+    private ImageView BtnCalendar;
+
+    @FXML
+    private ImageView BtnJournalList;
+
+    @FXML
+    private ImageView BtnMap;
+
+    @FXML
+    private ImageView BtnNewEntry;
+
+    @FXML
+    private Button BtnSFL;
+
+    @FXML
+    void SelectFileLocationClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ShowCalendarPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ShowJournalListPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ShowMapPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ShowNewEntryPage(MouseEvent event) {
+
+    }
+
+}

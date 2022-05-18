d28ce10006b78efb87c997a3dc81dcf6e890cc96
katri <katrin.tramberger@gmail.com>
2022-05-18 13:58:56 +0200

    #20 kategorien können bearbeitet und gelöscht werden #21 es können so viele struct infos wie möglich erzeugt werden

--- src/main/java/at/jku/se/diary/StructuredInfoController.java
+++ src/main/java/at/jku/se/diary/StructuredInfoController.java
@@ -1,30 +1,37 @@
 package at.jku.se.diary;
+
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import org.controlsfx.control.Rating;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static at.jku.se.diary.HelloFX.diary;
 
 public class StructuredInfoController implements Initializable {
 
+
         @FXML
         private Button BtnAdd;
 
         @FXML
         private Button BtnOk;
-
-        @FXML
-        private TableView<StructInformation> tableList;
 
         @FXML
         private ImageView btnNewEntry;
@@ -36,42 +43,86 @@ public class StructuredInfoController implements Initializable {
         private ImageView delete;
 
         @FXML
-        private TextField structuredText;
+        private ImageView journalView;
+
         @FXML
-        private Rating rating;
+        private TextField structuredText;
+
+        @FXML
+        private TableView<StructInformation> tableList;
+
+        @FXML
+        public Rating rating;
 
         private String selectedCategory;
 
-
         @FXML
         void AddToList(ActionEvent event) {
+                StructInformation structInfo = new StructInformation(tableList.getItems().size(), selectedCategory, rating.getRating() , structuredText.getText());
+                tableList.getItems().add(structInfo);
+                category.setValue(" ");
+                rating.setRating(0);
+                structuredText.setText("");
 
-                StructInformation structInfo = new StructInformation(0, selectedCategory, rating.getRating() , structuredText.getText());
-
-                TableColumn<StructInformation, String> category = new TableColumn<StructInformation, String>("Category");
-                category.setCellValueFactory(c -> new SimpleStringProperty((c.getValue().getCategory())));
-
-                TableColumn<StructInformation, Double> stars = new TableColumn<StructInformation, Double>("Stars");
-                stars.setCellValueFactory(c -> new SimpleObjectProperty(c.getValue().getStars()));
-
-                TableColumn<StructInformation, String> info = new TableColumn<StructInformation, String>("Information");
-                info.setCellValueFactory(c -> new SimpleObjectProperty(c.getValue().getStructuredText()));
-
-                tableList.getColumns().addAll(category, stars, info);
         }
 
         @FXML
-        void SaveListOnClick(ActionEvent event) {
+        void SaveListOnClick(ActionEvent event) throws IOException {
+                ArrayList<StructInformation> infos = new ArrayList<>();
+                infos.addAll(tableList.getItems());
+                diary.getEntryList().get(diary.getEntryList().size() -1).setStructuredInfo(infos);
+                diary.getEntryList().get(diary.getEntryList().size() -1).getStructuredInfo().forEach(info ->
+                        System.out.println(info.getCategory() + " " + info.getStars()));
+
+                Scene scene = btnNewEntry.getScene();
+                URL url = new File("src/main/java/at/jku/se/diary/DiaryEntryView.fxml").toURI().toURL();
+                Parent root = FXMLLoader.load(url);
+                scene.setRoot(root);
         }
 
         @FXML
         void showNewEntryPage(MouseEvent event) {
+
+        }
+
+        @FXML
+        void deleteStructuredInfo(MouseEvent event) {
+
+                tableList.getItems().remove(tableList.getSelectionModel().getSelectedItem().getId());
+
         }
 
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
-                category.getItems().addAll(diary.getCategories());
+                TableColumn<StructInformation, String> columnCategory = new TableColumn<StructInformation, String>("Category");
+                columnCategory.setCellValueFactory(c -> new SimpleStringProperty((c.getValue().getCategory())));
+
+                TableColumn<StructInformation, Double> columnStars = new TableColumn<StructInformation, Double>("Stars");
+                columnStars.setCellValueFactory(c -> new SimpleObjectProperty(c.getValue().getStars()));
+
+                TableColumn<StructInformation, String> columnInfo = new TableColumn<StructInformation, String>("Information");
+                columnInfo.setCellValueFactory(c -> new SimpleObjectProperty(c.getValue().getStructuredText()));
+
+                tableList.getColumns().addAll(columnCategory,columnStars, columnInfo);
+                ObservableList<StructInformation> diaryE = FXCollections.observableArrayList(diary.getEntryList().get(diary.getEntryList().size() -1).getStructuredInfo());
+                diaryE.forEach(info -> System.out.println(info.getCategory() + " " + info.getStars()));
+                if (diaryE.size()>0)tableList.setItems(diaryE);
+
+                //set categorie
+                category.getItems().addAll(HelloFX.diary.getCategories());
                 category.setOnAction(this::selectCategory);
+        }
+
+        @FXML
+        void openEditCategories(MouseEvent event) {
+                try {
+                        Scene scene = tableList.getScene();
+                        URL url = new File("src/main/java/at/jku/se/diary/CategoryList.fxml").toURI().toURL();
+                        Parent root = FXMLLoader.load(url);
+                        scene.setRoot(root);
+                } catch (Exception e) {
+                        System.out.println("load categories" + e);
+                }
         }
 
         public void selectCategory(ActionEvent event){

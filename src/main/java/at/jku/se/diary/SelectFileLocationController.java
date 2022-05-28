e06e81835f466f661acedd585c05d3a7a3b92a6c
lazicMichelle <mischilazi@gmail.com>
2022-05-28 18:55:11 +0200

    #13 speicherort festlegen

--- src/main/java/at/jku/se/diary/SelectFileLocationController.java
+++ src/main/java/at/jku/se/diary/SelectFileLocationController.java
@@ -7,29 +7,55 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
 
-import java.io.File;
-import java.io.IOException;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.*;
 import java.net.URL;
 
-public class SelectFileLocationController {
-    private String entryFileLocation;
-    private String photoFileLocation;
+public class SelectFileLocationController extends Component {
+    File diary = HelloFX.diaryFile;
 
     @FXML
     private Button btnSaveFileLocation;
 
     @FXML
-    private TextField txtEntryFileLocation;
+    private Button btnSearchLocationFile;
 
     @FXML
-    private TextField txtPhotoFileLocation;
+    private Button btnSearchLocationPhoto;
 
     @FXML
     void saveFileLocation(MouseEvent event) throws IOException {
-        //save the selected Location
-        entryFileLocation = txtEntryFileLocation.getText();
-        photoFileLocation = txtPhotoFileLocation.getText();
+        String path;
+        String filename;
+        String filecontent = diary.toString();
 
+        FileNameExtensionFilter filter = new FileNameExtensionFilter ("XML-File","XML");
+        JFileChooser saveas = new JFileChooser();
+        saveas.setDialogTitle("Save File at ...");
+        saveas.setFileFilter((javax.swing.filechooser.FileFilter) filter);
+
+        int userSelection = saveas.showSaveDialog(saveas);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            FileWriter fw = new FileWriter(saveas.getSelectedFile() + ".xml");
+            path = saveas.getSelectedFile().getAbsolutePath();
+            filename = saveas.getSelectedFile().getName();
+            //lb1.setText(path + " " + filename);
+
+            fw.write(filecontent);
+            fw.close();
+        }
+
+    }
+
+    @FXML
+    void savePhotoLocation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveSetLocations(MouseEvent event) throws IOException {
         //switch to JournalList Page
         Scene scene = btnSaveFileLocation.getScene();
         URL url = new File("src/main/java/at/jku/se/diary/JournalList.fxml").toURI().toURL();
